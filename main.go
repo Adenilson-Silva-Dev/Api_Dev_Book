d6ef6cfbf4097d17eae02715d019183a5229bc02
Adenilson-Silva-Dev <adenilsonrs3561@gmail.com>
2025-04-04 16:37:52 -0300

    Task config finalizada

--- main.go
+++ main.go
@@ -5,11 +5,15 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Adenilson-Silva-Dev/Api_Dev_Book/src/config"
 	"github.com/Adenilson-Silva-Dev/Api_Dev_Book/src/routes"
 )
 
 func main() {
 
+	// Carregar as variáveis de ambiente
+	config.Carregar()
+	fmt.Println(config.Porta)
 	r := routes.Gerar()
 	fmt.Println("Iniciando o servidor na porta 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
--- src/config/config.go
+++ src/config/config.go
@@ -1,10 +1,37 @@
 package config
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
 var (
 	StringConexaoComBanco = ""
 	Porta                 = 0
 )
 
-func Carregar(){
-	
+// funcão  vai ser responsável por carregar as variáveis de ambiente
+func Carregar() {
+
+	var erro error
+	if erro = godotenv.Load(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	// convertendo a porta para uma string usando o strconv.Atoi
+
+	Porta, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	StringConexaoComBanco = fmt.Sprintf("%s:%s@tcp(localhost:3305)/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USUARIO"),
+		os.Getenv("DB_SENHA"),
+		os.Getenv("DB_NOME"),
+	)
 }
